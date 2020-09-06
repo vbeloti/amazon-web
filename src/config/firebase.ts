cbf526f0bbb56257e79ce0d4fb2fe6c42d8d4850
unknown <viniciusbeloti7@gmail.com>
2020-09-06 09:08:22 -0300

    feat: amazon web base

---
+++ src/config/firebase.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD-iT9DVdq3sepsg4GrpYOmQAH_UTb2fT0",
+    authDomain: "web-9139a.firebaseapp.com",
+    databaseURL: "https://web-9139a.firebaseio.com",
+    projectId: "web-9139a",
+    storageBucket: "web-9139a.appspot.com",
+    messagingSenderId: "22661755101",
+    appId: "1:22661755101:web:003aec2811d504f4d2eb34",
+    measurementId: "G-YZMBBZWSWX"
+};
