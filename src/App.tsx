eff5c69784ab7e61bc9c74394c016465cf381368
unknown <viniciusbeloti7@gmail.com>
2020-09-06 09:57:02 -0300

    feat: add Home Styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
           </Route>
           <Route path="/">
             <Header />
-            <h1>Home</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
+import "./styles.css";
 
 const Home: React.FC = () => {
   return (
-      <div className="home">
-          Home
-      </div>
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-na.ssl-images-amazon.com/images/G/32/digital/video/merch/subs/benefit-id/s-z/servicelevel/heroes/asin2020/DESKTOP_TALL_HERO_1X_2707_Brazil_Service_Level_Campaign_1500X600._CB407407509_.jpg"
+        alt="Banner Home"
+      />
+    </div>
   );
-}
+};
 
 export default Home;
