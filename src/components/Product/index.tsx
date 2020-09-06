3e59316015c9134315c9c7de0f6029583c7883b2
unknown <viniciusbeloti7@gmail.com>
2020-09-06 10:15:17 -0300

    feat: add Product

---
+++ src/components/Product/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const Product: React.FC = () => {
+  return (
+      <div className="product">
+          
+      </div>
+  );
+}
+
+export default Product;
