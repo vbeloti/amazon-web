7d945b6a1643e7b811b7515fddaedd5d47b57073
unknown <viniciusbeloti7@gmail.com>
2020-09-06 09:15:43 -0300

    feat: add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      
-    </div>
+    <Router>
+      <div className="app">
+      <Switch>
+        <Route path="/checkout">
+            <h1>Checkout</h1>
+        </Route>
+        <Route path="/login">
+            <h1>Login Page</h1>
+        </Route>
+        <Route path="/">
+            <h1>Home</h1>
+        </Route>
+      </Switch>
+      </div>
+
+    </Router>
+    
   );
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header: React.FC = () => {
+  return (
+      <div className="header">
+          Header
+      </div>
+  );
+}
+
+export default Header;
